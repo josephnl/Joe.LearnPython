c039a4d43a6d46d3b4b8e856f49a76e89271b6a3
josephnl <josephnl@foxmail.com>
2016-05-16 22:01:41 +0800

    mac learn python

--- filter.py
+++ filter.py
@@ -3,7 +3,7 @@
 # Filename filter.py
 # filter 学习，打印1000以内素数
 
-def _ord_inter():
+def _ord_intr():
     n = 1
     while True:
         n = n + 2
@@ -12,17 +12,23 @@ def _ord_inter():
 def _not_divisible(n):
     return lambda x: x % n > 0
 
+for n in _ord_intr():
+    if n < 50:
+        f = _not_divisible(n)
+        print(f)
+    else:
+        break
 
 def primes():
     yield 2
-    it = _ord_inter() #初始序列
+    it = _ord_intr() #初始序列
     while True:
         n = next(it) #给出序列的第一个素数
         yield n
         it = filter(_not_divisible(n), it) #用新生成的素数给出新的序列
-
-for i in primes():
-    if i < 100:
-        print(i)
+#
+for n in primes():
+    if n < 10:
+        print(n)
     else:
         break
