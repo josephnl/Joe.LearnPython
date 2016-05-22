708d72383e5496692592739e320e80f207120626
josephnl <josephnl@foxmail.com>
2016-05-23 02:30:38 +0800

    学习spider

---
+++ decode.py
@@ -0,0 +1,18 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename decode.py
+
+import time
+
+s = "徐骏 测试 encoding 功能"  # 最终要写到文件里的内容
+print(type(s))
+
+print()
+#
+# s1 = s.encode('ascii')
+# print(type(s1))
+# print(s1)
+#
+# s2 = s1.decode('utf-8')
+# print(type(s2))
+# print(s2)
